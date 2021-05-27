c3b60a488a6e88a36a6316c9bb0de891debaa943
vVitalv <71068175+vVitalv@users.noreply.github.com>
2021-05-27 15:46:30 +0300

    Update tailwind.config.js
    screen media queries added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,17 @@
 module.exports = {
   purge: ['./client/**/*.html', './client/**/*.jsx', './client/**/*.js'],
-  theme: {},
+  theme: {
+    screen: {
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    }
+  },
   variants: {},
   plugins: []
 }
