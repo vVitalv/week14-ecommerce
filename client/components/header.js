2a306b8d8528286be67b024b97fdc564b458ed80
vVitalv <vitalvital@bk.ru>
2021-06-27 17:12:28 +0300

    logotype component replaced to separate module

--- client/components/header.js
+++ client/components/header.js
@@ -1,30 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
+import Logotype from './logotype'
 import CurrencyPanel from './currency-panel'
 import BasketButton from './btn-basket'
 import SortMenu from './menu-sorting'
-import logo from './src/logo-2.png'
 
 const Header = () => {
   return (
     <header>
       <div className="header-menu">
-        <Link to="/" className="logo-link">
-          <img src={logo} alt="Logo" className="logo" />
-          <div className="logo-text">
-            <span className="logo-letter logo-letter_red">E</span>
-            <span className="logo-letter logo-letter_indigo">-</span>
-            <span className="logo-letter logo-letter_purple">C</span>
-            <span className="logo-letter logo-letter_green">O</span>
-            <span className="logo-letter logo-letter_blue">M</span>
-            <span className="logo-letter logo-letter_pink">M</span>
-            <span className="logo-letter logo-letter_red">E</span>
-            <span className="logo-letter logo-letter_indigo">R</span>
-            <span className="logo-letter logo-letter_purple">C</span>
-            <span className="logo-letter logo-letter_green">E</span>
-          </div>
-        </Link>
+        <Logotype />
         <CurrencyPanel />
         <BasketButton />
       </div>
---
+++ client/components/logotype.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import logo from './src/logo-2.png'
+
+const Logotype = () => {
+  return (
+    <Link to="/" className="logo-link">
+      <img src={logo} alt="Logo" className="logo" />
+      <div className="logo-text">
+        <span className="logo-letter logo-letter_red">E</span>
+        <span className="logo-letter logo-letter_indigo">-</span>
+        <span className="logo-letter logo-letter_purple">C</span>
+        <span className="logo-letter logo-letter_green">O</span>
+        <span className="logo-letter logo-letter_blue">M</span>
+        <span className="logo-letter logo-letter_pink">M</span>
+        <span className="logo-letter logo-letter_red">E</span>
+        <span className="logo-letter logo-letter_indigo">R</span>
+        <span className="logo-letter logo-letter_purple">C</span>
+        <span className="logo-letter logo-letter_green">E</span>
+      </div>
+    </Link>
+  )
+}
+
+Logotype.propTypes = {}
+
+export default React.memo(Logotype)
