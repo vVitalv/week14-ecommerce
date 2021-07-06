0578107749c449c3b8aaac3ccce8a02c66f11da1
vVitalv <vitalvital@bk.ru>
2021-07-06 15:09:52 +0300

    basket-empty source place fixed

--- client/components/cart/basket-empty.js
+++ client/components/cart/basket-empty.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import basketNeedEat from '../src/basket-eat.gif'
+import basketNeedEat from '../../assets/images/basket-eat.gif'
 
 const BasketEmpty = () => {
   return (
