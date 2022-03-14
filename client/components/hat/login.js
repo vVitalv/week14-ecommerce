3e96f567a998c449ac7ef237b05b9b9f5a904afa
vVitalv <vitalvital@bk.ru>
2022-03-14 11:17:43 +0300

    LoginForm portal

--- client/components/hat/login.js
+++ client/components/hat/login.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { createPortal } from 'react-dom'
+
+import Button from '../btns/btn'
+import LoginForm from './auth/auth-form'
 
 const Login = () => {
-  return <div className="login">Login</div>
+  const [isPortalOpen, setPortalOpen] = useState(false)
+  return (
+    <div className="login">
+      <Button
+        operation="login"
+        sign="Sign in"
+        className="login-btn"
+        data=""
+        onClickFunction={() => setPortalOpen(true)}
+      />
+      {isPortalOpen && createPortal(<LoginForm setPortalOpen={setPortalOpen} />, document.body)}
+    </div>
+  )
 }
 
 Login.propTypes = {}
